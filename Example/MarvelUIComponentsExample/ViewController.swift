94ff965f96689237f5c9d030a88bdb88233fbe2a
Kevin Sabajanes <kevin.sabajanes@talentomobile.com>
2021-11-22 21:33:43 +0100

    Init
    Init

---
+++ Example/MarvelUIComponentsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelUIComponentsExample
+//
+//  Created by K3V1NS4N on 22/11/21.
+//
+
+import UIKit
+import MarvelUIComponents
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
